feaa417df9b19120ef13281ca5149f2f6ed2ff38
hainh1712 <nguyenhoanghai20021712@gmail.com>
2023-11-18 00:08:10 +0700

    api create user

--- server/main.py
+++ server/main.py
@@ -33,6 +33,25 @@ app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
 async def root():
     return {"message": "hello world"}
 
+@app.get('/posts/')
+async def post():
+    post = db.session.query(ModelPosts).all()
+    return post
+
+@app.post('/posts/', response_model=SchemaPosts)
+async def post(book: SchemaPosts):
+    db_post = ModelPosts(user_id=book.user_id, title=book.title, description=book.description, image_url=book.image_url)
+    db.session.add(db_post)
+    db.session.commit()
+    return db_post
+
+@app.post('/users/', response_model=SchemaUsers)
+async def user(user: SchemaUsers):
+    db_user = ModelUsers(name=user.name, username=user.username, password=user.password, classname=user.classname, grade=user.grade, avatar_url=user.avatar_url, cover_image_url=user.cover_image_url)
+    db.session.add(db_user)
+    db.session.commit()
+    return db_user
+
 # To run locally
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
